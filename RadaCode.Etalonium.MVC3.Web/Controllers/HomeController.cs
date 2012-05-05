572bd578368d24bffe2baa172e5f12ded80ac86c
Max Pavlov <max@radacode.com>
2012-05-06 00:59:22 +0300

    Initial commit

---
+++ RadaCode.Etalonium.MVC3.Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace RadaCode.Etalonium.MVC3.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ RadaCode.Etalonium.MVC3.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RadaCode.Etalonium.MVC3.Web.MvcApplication" Language="C#" %>
